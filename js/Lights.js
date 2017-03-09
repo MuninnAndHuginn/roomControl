8944d4a79d38ac59eb6ce3f1bf5a95d21c622503
Jordan Porter <porter.jordan@gmail.com>
2017-03-08 21:58:31 -0500

    Refactor the message handling interface.

--- js/Lights.js
+++ js/Lights.js
@@ -1,70 +1,62 @@
 import React from 'react';
 
 // Connect to the lights namespace of the backend.
-var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port + '/lights');
+var light_socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port + '/lights');
 
-class LightsBackend {
-   
-    constructor(socket) {
-        this.groupCallbacks = {};
-        this.fullUpdateCallback = null;
-
-        socket.on('group_update', this.handleGroupUpdate)
-        socket.on('event', this.handleEvent)
-        socket.on('connect', this.connected);
-        socket.on('disconnect', this.disconnected);
-    }
-   
-    connected() {
-        console.log("Connected to lights backend!");
-    }
-
-    disconnected() {
-        console.log("Disconnected from lights backend!");
-    }
-
-
-    registerGroupCallback(id, fn) {
-        this.groupCallbacks[id] = fn;
-    }
-
-    registerFullUpdateCallback(fn) {
-        this.fullUpdateCallback = fn;
-    }
-
-    processGroupsUpdate(groups) {
-        for (let group of groups) {
-            if (this.groupCallbacks[group.id]) {
-                this.groupCallbacks[group.id](group);
-            }
-        }
-    }
-
-    processFullUpdate(groups, lights) {
-        if (this.fullUpdateCallback) {
-            this.fullUpdateCallback(groups, lights);
-        }
-
-        this.processGroupsUpdate(groups);
-    }
-
-    handleGroupUpdate(msg) {
-        var id = msg['id'];
-        var group = msg;
-        if (this.groupCallbacks[id]) {
-            this.groupCallbacks[id](group);
-        }
-    }
-
-    handleEvent(msg) {
-        if (msg.id == 'HUE') {
-            this.processFullUpdate(msg.data['groups'], msg.data['lights']);
-        }
-    }
+$.LightsBackend = {
+    groupCallbacks : {},
+    fullUpdateCallback: null
 }
+    
+$.LightsBackend.connected = function() {
+    console.log("Connected to lights backend!");
+};
 
-// Used by the React components to listen for event updates.
-var lbe = new LightsBackend(socket);
+$.LightsBackend.disconnected = function() {
+    console.log("Disconnected from lights backend!");
+};
+
+$.LightsBackend.registerGroupCallback = function(id, fn) {
+    $.LightsBackend.groupCallbacks[id] = fn;
+};
+
+$.LightsBackend.registerFullUpdateCallback = function(fn) {
+    $.LightsBackend.fullUpdateCallback = fn;
+};
+
+$.LightsBackend.processGroupsUpdate = function(groups) {
+    for (let group of groups) {
+        if ($.LightsBackend.groupCallbacks[group.id]) {
+            $.LightsBackend.groupCallbacks[group.id](group);
+        }
+    }
+};
+
+$.LightsBackend.processFullUpdate = function(groups, lights) {
+    if ($.LightsBackend.fullUpdateCallback) {
+        $.LightsBackend.fullUpdateCallback(groups, lights);
+    }
+    $.LightsBackend.processGroupsUpdate(groups);
+};
+
+$.LightsBackend.handleGroupUpdate = function(msg) {
+    var id = msg['id'];
+    var group = msg;
+    if ($.LightsBackend.groupCallbacks[id]) {
+        $.LightsBackend.groupCallbacks[id](group);
+    }
+};
+
+$.LightsBackend.handleEvent = function(msg) {
+    if (msg.id == 'HUE') {
+        $.LightsBackend.processFullUpdate(msg.data['groups'], msg.data['lights']);
+    }
+};
+
+light_socket.on('group_update', $.LightsBackend.handleGroupUpdate)
+light_socket.on('event', $.LightsBackend.handleEvent)
+light_socket.on('connect', $.LightsBackend.connected);
+light_socket.on('disconnect', $.LightsBackend.disconnected);
 
 class LightGroup extends React.Component {
     constructor(props) {
@@ -95,14 +87,14 @@ class LightGroup extends React.Component {
         this.handleClick = this.handleClick.bind(this);
         this.handleGroupUpdate = this.handleGroupUpdate.bind(this);
 
-        lbe.registerGroupCallback(this.state.myId, this.handleGroupUpdate);
+        $.LightsBackend.registerGroupCallback(this.state.myId, this.handleGroupUpdate);
    }
 
     handleClick() {
         // Send the opposite of the current on state.
         var wantState = !this.state.isOn;
         var myId = this.state.myId;
-        socket.emit('group_click', {id: myId, on: wantState});
+        light_socket.emit('group_click', {id: myId, on: wantState});
     }
 
     handleGroupUpdate(group) {
@@ -110,8 +102,8 @@ class LightGroup extends React.Component {
 
         name = group.name;
         color = group.action.color;
-        var anyon = (msg.state.any_on == true);
-        var allon = (msg.state.all_on == true);
+        var anyon = (group.state.any_on == true);
+        var allon = (group.state.all_on == true);
 
         // Assume on, check just below.
         isOn = true;
@@ -174,16 +166,16 @@ class Lights extends React.Component {
         super(props);
 
         this.handleFullUpdate = this.handleFullUpdate.bind(this);
-        lbe.registerFullUpdateCallback(this.handleFullUpdate);
+        $.LightsBackend.registerFullUpdateCallback(this.handleFullUpdate);
 
         this.state = {groups: null, lights: null};
     }
 
-    handleFullUpdate(groups, lights) {
+    handleFullUpdate(_groups, _lights) {
         var {groups, lights} = this.state;
 
-        groups = groups;
-        lights = lights;
+        groups = _groups;
+        lights = _lights;
 
         this.setState({groups, lights});
     }
