e2b3a545145da10f018178e5f7487350d95fc58a
Jordan Porter <porter.jordan@gmail.com>
2018-12-24 14:51:53 -0500

    add group support. fetching all groups causes hue bridge to break connection due to too many requests... will have to use only full state to get groups.

--- src/controllers/lightController.js
+++ src/controllers/lightController.js
@@ -17,7 +17,6 @@ function getLightById(id) {
             on:result.state.on,
             bri:result.state.bri
         })
-        console.log("Return getLightById: " + singleLight)
         return singleLight
     })
 }
@@ -36,6 +35,20 @@ function setLightOnOffById(id, on) {
     })
 }
 
+function setGroupOnOffById(id, on) {
+    var state;
+    if (on) {
+        state = lightState.create().on()
+    } else {
+        state = lightState.create().off()
+    }
+    return hueapi.setGroupLightState(id, state).then((result)=> {
+        console.log(result)
+    }).then((result)=> {
+        return getGroupById(id)
+    })
+}
+
 function getAllLights() {
     return hueapi.getGroup(0).then((result)=>{
         console.log(result)
@@ -47,19 +60,124 @@ function getAllLights() {
         })
 
         return Promise.all(promises).then((values)=>{
-            console.log("Return getAllLights: " + values)
             return values
         })
     })
 }
+
+function getGroupById(id) {
+    return hueapi.getGroup(id).then((result)=>{
+        console.log(result)
+
+        promises = []
+
+        result['lights'].forEach((id)=>{
+            promises.push(getLightById(id))
+        })
+
+        singleGroup = {id:id, name:result.name, lights:[]}
+
+        return Promise.all(promises).then((values)=>{
+            singleGroup.lights = values
+            return singleGroup
+        })
+    })
+}
+
+function getAllGroups() {
+    return hueapi.groups().then((result)=>{
+        console.log(result)
+
+        promises = []
+
+        for (var id in result) { 
+            if (id != 0) {
+                promises.push(getGroupById(id))
+            }
+        }
+
+        return Promise.all(promises).then((values)=>{
+            return values
+        })
+    })
+}
+
+function getHueState() {
+    return hueapi.getFullState().then((result)=>{
+        console.log(result)
+        return result    
+    }).then((result)=>{
+        fullData = {groups: [], lights: []}
+
+        for (var id in result.lights) {
+            singleLight = Light.parse({
+                id:id,
+                name:result.lights[id].name,
+                on:result.lights[id].state.on,
+                bri:result.lights[id].state.bri
+            })
+            fullData.lights.push(singleLight)
+        }
+
+        for (var id in result.groups) {
+            singleGroup = {
+                id:id,
+                lights:[],
+                all_on: result.groups[id].state.all_on,
+                any_on: result.groups[id].state.any_on
+            }
+
+            result.groups[id].lights.forEach((id)=>{
+                singleGroup.lights.push(fullData.lights[id])
+            })
+            fullData.groups.push(singleGroup)
+        }
+
+        return fullData
+    })
+}
+
 
 // Get all lights
 exports.getLights = async (req, reply) => {
     try {
         return getAllLights().then((result)=>{
             allLights = {lights: result}
-            console.log("API getLights: " + JSON.stringify(allLights))
+            console.log("API getLights: " + JSON.stringify(allLights, null, 2))
             return allLights
+        },
+        (err)=> {
+            throw err
+        })
+    } catch (err) {
+        throw boom.boomify(err)
+    }
+}
+
+// Get all light groups
+exports.getGroups = async (req, reply) => {
+    try {
+        return getAllGroups().then((result)=>{
+            allGroups = {groups: result}
+            console.log("API getGroups: " + JSON.stringify(allGroups, null, 2))
+            return allGroups
+        },
+        (err)=> {
+            throw err
+        })
+    } catch (err) {
+        throw boom.boomify(err)
+    }
+}
+
+
+// Get full state
+exports.getFullState = async (req, reply) => {
+    try {
+        return getHueState().then((result)=>{
+            fullState = result
+            console.log("API getFullState: " + JSON.stringify(fullState, null, 2))
+            return fullState
         },
         (err)=> {
             throw err
@@ -75,7 +193,7 @@ exports.getSingleLight = async (req, reply) => {
         const id = req.params.id
         return getLightById(id).then((result)=>{
             singleLight = result
-            console.log("API getSingleLight: " + JSON.stringify(singleLight))
+            console.log("API getSingleLight: " + JSON.stringify(singleLight, null, 2))
             return singleLight
         },
         (err)=> {
@@ -86,7 +204,24 @@ exports.getSingleLight = async (req, reply) => {
     }
 }
 
-// Set single line on/off by ID
+// Get single light by ID
+exports.getSingleGroup = async (req, reply) => {
+    try {
+        const id = req.params.id
+        return getGroupById(id).then((result)=>{
+            singleGroup = result
+            console.log("API getSingleGroup: " + JSON.stringify(singleGroup, null, 2))
+            return singleGroup
+        },
+        (err)=> {
+            throw err
+        })
+    } catch (err) {
+        throw boom.boomify(err)
+    }
+}
+
+// Set single group on/off by ID
 exports.setSingleLightOnOff = async (req, reply) => {
     try {
         const id = req.params.id
@@ -94,8 +229,27 @@ exports.setSingleLightOnOff = async (req, reply) => {
         const on = ("on" == onOff)
         return setLightOnOffById(id, on).then((result)=>{
             singleLight = result
-            console.log("API setSingleLightOnOff: " + JSON.stringify(singleLight))
+            console.log("API setSingleLightOnOff: " + JSON.stringify(singleLight, null, 2))
             return singleLight
+        },
+        (err)=> {
+            throw err
+        })
+    } catch (err) {
+        throw boom.boomify(err)
+    }
+}
+
+// Set single group on/off by ID
+exports.setSingleGroupOnOff = async (req, reply) => {
+    try {
+        const id = req.params.id
+        const onOff = req.params.which
+        const on = ("on" == onOff)
+        return setGroupOnOffById(id, on).then((result)=>{
+            singleGroup = result
+            console.log("API setSingleGroupOnOff: " + JSON.stringify(singleGroup, null, 2))
+            return singleGroup
         },
         (err)=> {
             throw err
--- src/routes/index.js
+++ src/routes/index.js
@@ -9,13 +9,34 @@ const routes = [
     },
     {
         method: 'GET',
-        url: '/api/lights/:id',
+        url: '/api/lights/groups',
+        handler: lightController.getGroups
+    },
+
+    {
+        method: 'GET',
+        url: '/api/lights/fullState',
+        handler: lightController.getFullState
+    },
+    {
+        method: 'GET',
+        url: '/api/lights/light/:id',
         handler: lightController.getSingleLight
     },
     {
         method: 'GET',
-        url: '/api/lights/:id/:which(on|off)',
+        url: '/api/lights/group/:id',
+        handler: lightController.getSingleGroup
+    },
+    {
+        method: 'GET',
+        url: '/api/lights/light/:id/:which(on|off)',
         handler: lightController.setSingleLightOnOff
+    },
+    {
+        method: 'GET',
+        url: '/api/lights/group/:id/:which(on|off)',
+        handler: lightController.setSingleGroupOnOff
     }
 ]
 
